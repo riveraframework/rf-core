4d834dc259545b8444d7e16a955e70e99689ee71
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2018-06-20 10:38:44 -0400

    Improve Waston Assistant wrapper

--- src/External/IBM/WatsonAssistant.php
+++ src/External/IBM/WatsonAssistant.php
@@ -9,7 +9,9 @@
  */
 
 namespace Rf\Core\External\IBM;
+
 use Rf\Core\Http\Curl;
+use Rf\Core\Http\Exceptions\CurlException;
 
 /**
  * Class WatsonAssistant
@@ -18,16 +20,35 @@ use Rf\Core\Http\Curl;
  */
 class WatsonAssistant {
 
+    /** Watson API url */
     const API_URL = 'https://gateway.watsonplatform.net/assistant/api/v1/';
+
+    /** Supported endpoint list */
     const ENDPOINTS = [
-        'message-post' => '/workspaces/[WORKSPACE_ID]/message/'
+        'message-send' => '/workspaces/[WORKSPACE_ID]/message/',
+        'entities-update' => '/workspaces/[WORKSPACE_ID]/entities/[ENTITY]',
     ];
 
+    /** @var string $username */
     protected $username;
+
+    /** @var string $password */
     protected $password;
+
+    /** @var string $workspaceId */
     protected $workspaceId;
+
+    /** @var string $version */
     protected $version = '2018-02-16';
 
+    /**
+     * WatsonAssistant constructor.
+     *
+     * @param string $username
+     * @param string $password
+     * @param string $workspaceId
+     * @param string $version
+     */
     public function __construct($username, $password, $workspaceId, $version = null) {
 
         $this->username = $username;
@@ -40,29 +61,66 @@ class WatsonAssistant {
 
     }
 
-    public function getUrl($method, $endpoint) {
+    /**
+     * Generate the final endpoint url
+     *
+     * @param string $endpoint
+     * @param array $queryVars
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function getUrl($endpoint, $queryVars = []) {
 
-        if(!in_array($endpoint . '-' . $method, array_keys(static::ENDPOINTS))) {
+        if(!in_array($endpoint, array_keys(static::ENDPOINTS))) {
             throw new \Exception('Endpoint not supported');
         }
 
-        return static::API_URL . str_replace(
-            '[WORKSPACE_ID]',
-            $this->workspaceId,
-            static::ENDPOINTS[$endpoint . '-' . $method]
-        ) . '?version=' . $this->version;
+        $url = static::API_URL . str_replace(
+                '[WORKSPACE_ID]',
+                $this->workspaceId,
+                static::ENDPOINTS[$endpoint]
+            ) . '?version=' . $this->version;
+
+        foreach ($queryVars as $name => $value) {
+            $url = str_replace('[' . $name . ']', $value, $url);
+        }
+
+        return $url;
 
     }
 
-    public function post($endpoint, $data) {
+    /**
+     * Perform a POST request against the API
+     *
+     * @param string $endpoint
+     * @param string|array $data
+     * @param array $queryVars
+     *
+     * @return false|string
+     * @throws \Exception
+     */
+    public function post($endpoint, $data, $queryVars = []) {
 
-        return $this->getResponse('post', $endpoint, $data);
+        return $this->getResponse('post', $endpoint, $data, $queryVars);
 
     }
 
-    protected function getResponse($method, $endpoint, $data) {
+    /**
+     * Get the response from the API
+     *
+     * @param string $method
+     * @param string $endpoint
+     * @param string|array $data
+     * @param array $queryVars
+     *
+     * @return false|string
+     * @throws \Exception
+     * @throws CurlException
+     */
+    protected function getResponse($method, $endpoint, $data, $queryVars = []) {
 
-        $request = new Curl($this->getUrl($method, $endpoint));
+        $request = new Curl($this->getUrl($endpoint, $queryVars));
         $request->setMethod($method);
         $request->addHeader('Content-Type: application/json');
         $request->setOption(CURLOPT_USERPWD, $this->username . ':' . $this->password);
