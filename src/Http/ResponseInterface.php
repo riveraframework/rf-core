dc67344c35b7db78e04e4d3810dbadf6e1601b57
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2019-06-02 17:34:04 -0400

    Remove function from response interface for me flexibility

--- src/Http/ResponseInterface.php
+++ src/Http/ResponseInterface.php
@@ -4,13 +4,11 @@ namespace Rf\Core\Http;
 
 /**
  * Interface ResponseInterface
+ *
  * @package Rf\Core\Http
  */
 interface ResponseInterface {
 
-    public function __construct($httpCode = 200);
-    public function addHeader($option, $value);
-    public function setBody($body);
     public function send();
 
 }
